68c9ac092ed87dcab0dfdcfac9f0340e7e2eb1a8
Sam <samirsuperman786@users.noreply.github.com>
2018-04-09 12:45:53 -0400

    added volume recognition

--- Listener.py
+++ Listener.py
@@ -8,29 +8,43 @@ import time
 import speech_recognition as sr
 import queue
 import pyaudio
+import numpy as np
+import sounddevice as sd
 
-r = None
-m = None
+global soundLevel
+soundLevel = [100]
+volumeMultiplier = 2
 q = queue.Queue()
-q.put("hi")
 
-def initializeSpeech():
+def initializeListener():
     r = sr.Recognizer()
     m = sr.Microphone()
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
-        r.listen_in_background(m, callback, 1.8)
-    #for _ in range(50): time.sleep(0.1)
+        r.adjust_for_ambient_noise(source) 
+        r.listen_in_background(m, translate, 1)
 
-# this is called from the background thread
-def callback(recognizer, audio):
-    # received audio data, now we'll recognize it using Google Speech Recognition
+def translate(recognizer, audio):
     try:
         word = recognizer.recognize_google(audio)
         print(word)
         q.put(word)
-
     except sr.UnknownValueError:
         print("...")
     except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print("Google Speech Recognition not configured; {0}".format(e))
+
+def volumeListener(indata, frames, time, status):
+    global soundLevel
+    volume_norm = np.linalg.norm(indata) * 10 * volumeMultiplier
+    soundLevel.append(int(volume_norm))
+    if(len(soundLevel)>100):
+        soundLevel.pop(0)
+
+def getSoundVolume():
+    return max(soundLevel)
+
+def soundVolume():
+    stream = sd.InputStream(callback=volumeListener)
+    with stream: 
+        while True:
+            sd.sleep(10 *1000)
