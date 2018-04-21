3a05d92bd601ab585990cc39c5e7760f865f6733
Sam <samirsuperman786@users.noreply.github.com>
2018-04-21 15:20:34 -0400

    moved scene handler

---
+++ Graphics/SceneManager.py
@@ -0,0 +1,46 @@
+from panda3d.core import *
+def setupScene(displayInstance, render):
+	#add one light per face, so each face is nicely illuminated
+	plight1 = PointLight('plight')
+	plight1.setColor(VBase4(1, 1, 1, 1))
+	plight1NodePath = render.attachNewNode(plight1)
+	plight1NodePath.setPos(0, 0, 500)
+	render.setLight(plight1NodePath)
+
+	plight2 = PointLight('plight')
+	plight2.setColor(VBase4(1, 1, 1, 1))
+	plight2NodePath = render.attachNewNode(plight2)
+	plight2NodePath.setPos(0, 0, -500)
+	render.setLight(plight2NodePath)
+
+	plight3 = PointLight('plight')
+	plight3.setColor(VBase4(1, 1, 1, 1))
+	plight3NodePath = render.attachNewNode(plight3)
+	plight3NodePath.setPos(0, -500, 0)
+	render.setLight(plight3NodePath)
+
+	plight4 = PointLight('plight')
+	plight4.setColor(VBase4(1, 1, 1, 1))
+	plight4NodePath = render.attachNewNode(plight4)
+	plight4NodePath.setPos(0, 500, 0)
+	render.setLight(plight4NodePath)
+
+	plight5 = PointLight('plight')
+	plight5.setColor(VBase4(1, 1, 1, 1))
+	plight5NodePath = render.attachNewNode(plight5)
+	plight5NodePath.setPos(500,0, 0)
+	render.setLight(plight5NodePath)
+
+	plight6 = PointLight('plight')
+	plight6.setColor(VBase4(1, 1, 1, 1))
+	plight6NodePath = render.attachNewNode(plight6)
+	plight6NodePath.setPos(-500,0, 0)
+	render.setLight(plight6NodePath)
+
+	# Load the environment model.
+	displayInstance.scene = displayInstance.loader.loadModel("models/environment")
+	# Reparent the model to render.
+	displayInstance.scene.reparentTo(displayInstance.render)
+	# Apply scale and position transforms on the model.
+	displayInstance.scene.setScale(0.25, 0.25, 0.25)
+	displayInstance.scene.setPos(-20, 50, 0)
--- __init__.py
+++ __init__.py
@@ -16,6 +16,7 @@ from Listener.VolumeReader import *
 from threading import Thread
 from Graphics.Word import *
 from Graphics.CloudManager import *
+from Graphics.SceneManager import *
 
 import socket
 import threading
@@ -52,63 +53,19 @@ class Display(ShowBase):
         self.otherPlayers = dict()
         self.myPID = None
         #load all the things
-        self.loadBackground() # load lights and the fancy background
+        setupScene(self, render)# load lights and the fancy background
         self.loadModels()
         loadClouds(render)
         #key movement
         self.createKeyControls()
         self.keyMap = {}
         #timerFired
-        taskMgr.doMethodLater(.2, self.update, "update")
-        taskMgr.doMethodLater(1, self.getNewWord, "word")
-        taskMgr.doMethodLater(.01, moveClouds, "cloud")
-
-    #loads the background
-    def loadBackground(self):
-        #add one light per face, so each face is nicely illuminated
-        plight1 = PointLight('plight')
-        plight1.setColor(VBase4(1, 1, 1, 1))
-        plight1NodePath = render.attachNewNode(plight1)
-        plight1NodePath.setPos(0, 0, 500)
-        render.setLight(plight1NodePath)
-
-        plight2 = PointLight('plight')
-        plight2.setColor(VBase4(1, 1, 1, 1))
-        plight2NodePath = render.attachNewNode(plight2)
-        plight2NodePath.setPos(0, 0, -500)
-        render.setLight(plight2NodePath)
-
-        plight3 = PointLight('plight')
-        plight3.setColor(VBase4(1, 1, 1, 1))
-        plight3NodePath = render.attachNewNode(plight3)
-        plight3NodePath.setPos(0, -500, 0)
-        render.setLight(plight3NodePath)
-
-        plight4 = PointLight('plight')
-        plight4.setColor(VBase4(1, 1, 1, 1))
-        plight4NodePath = render.attachNewNode(plight4)
-        plight4NodePath.setPos(0, 500, 0)
-        render.setLight(plight4NodePath)
-
-        plight5 = PointLight('plight')
-        plight5.setColor(VBase4(1, 1, 1, 1))
-        plight5NodePath = render.attachNewNode(plight5)
-        plight5NodePath.setPos(500,0, 0)
-        render.setLight(plight5NodePath)
-
-        plight6 = PointLight('plight')
-        plight6.setColor(VBase4(1, 1, 1, 1))
-        plight6NodePath = render.attachNewNode(plight6)
-        plight6NodePath.setPos(-500,0, 0)
-        render.setLight(plight6NodePath)
-
-        # Load the environment model.
-        self.scene = self.loader.loadModel("models/environment")
-        # Reparent the model to render.
-        self.scene.reparentTo(self.render)
-        # Apply scale and position transforms on the model.
-        self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(-20, 50, 0)
+        updateTimer = .2
+        newWordTimer = 1
+        moveCloudTimer = .01
+        taskMgr.doMethodLater(updateTimer, self.update, "update")
+        taskMgr.doMethodLater(newWordTimer, self.getNewWord, "word")
+        taskMgr.doMethodLater(moveCloudTimer, moveClouds, "cloud")
 
     def loadModels(self):
         pass
